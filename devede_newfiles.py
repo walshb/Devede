e7480fca77249601436c4334b8e1210d63606434
Ben Walsh <b@wumpster.com>
2011-12-05 21:19:23 +0000

    newfile.__init__ calls superclass __init__.

--- devede_newfiles.py
+++ devede_newfiles.py
@@ -180,7 +180,9 @@ class newfile(file_get_params):
 		""" This class manages every new film added. It reads its parameters (resolution, FPS, number of
 		channels...) and allows to generate the default values, both when choosing manually a file from the
 		Properties window, or when dragging&dropping them into the main window """
-		
+
+		file_get_params.__init__(self)
+
 		self.pal=pal
 		self.disctocreate=disctocreate
 		self.file_values=None
